4c011a13639190238748ac9b1a752025d1374da3
Smell-good-tang <155508961+Smell-good-tang@users.noreply.github.com>
2024-06-17 22:43:56 +0800

    Refining...
    Refining...

--- StairVselevator/efloors.cpp
+++ StairVselevator/efloors.cpp
@@ -83,143 +83,113 @@ void efloors::messagebox_common(const QString &title, const QString &text)
     delete box;
 }
 
+void efloors::closeEvent(QCloseEvent *event)
+{
+    QMessageBox *box = new QMessageBox(QMessageBox::Information, "再见！", "谢谢使用！", QMessageBox::NoButton, this);
+    QTimer::singleShot(3000, box, SLOT(accept()));
+    box->setFont(font_default);
+    box->exec();
+    delete box;
+}
+
 // 计算
 void efloors::on_btn_calculate_clicked()
 {
-    // 还没有输入有效总楼层数，并按按钮2
+    // 检查标签是否隐藏，表明尚未输入楼层总数
     if (ui->label->isHidden()) {
         messagebox_common("注意！", "请先输入有效总楼层数并按按钮2！");
+        return;
     }
 
-    // 已经输入有效总楼层数，并按按钮2
-    else if (!ui->label->isHidden()) {
-        int curtotal = ui->li_1->text().toInt();
+    int curtotal = ui->li_1->text().toInt();
 
-        // 按按钮2后，没有重新输入不同的新总楼层数
-        if (curtotal == total_8f) {
-            int start_8f = ui->li_2->text().toInt();  // 获取开始层层数
-            int end_8f   = ui->li_3->text().toInt();  // 获取目的层层数
+    // 检查输入的总楼层数是否与之前输入的相同
+    if (curtotal != total_8f) {
+        messagebox_common("注意！", "您已经更换了总楼层，请先点击按钮2得到可赋值的楼层！");
+        return;
+    }
 
-            if (ui->li_2->text().isEmpty()) {
-                messagebox_common("注意！", "开始层不能为空。");
-            } else if (ui->li_3->text().isEmpty()) {
-                messagebox_common("注意！", "目的层不能为空。");
-            } else if (start_8f <= 0 || end_8f <= 0) {
-                messagebox_common("注意！", "开始层和目的层均不能为负层。");
-            } else if (start_8f > total_8f || end_8f > total_8f) {
-                messagebox_common("注意！", "开始层和目的层均不能超出总层数。");
-            } else {
-                // 当数组不再需要时，释放内存
-                delete[] f_8f;
-                delete[] k_8f;
+    int start_8f = ui->li_2->text().toInt();
+    int end_8f   = ui->li_3->text().toInt();
 
-                // 将指针设置为nullptr，避免出现悬挂指针
-                f_8f = nullptr;
-                k_8f = nullptr;
+    if (ui->li_2->text().isEmpty() || ui->li_3->text().isEmpty()) {
+        messagebox_common("注意！", "开始层和目的层不能为空。");
+        return;
+    }
 
-                k_8f = new int[total_8f + 1];
-                f_8f = new bool[total_8f + 1];
+    if (start_8f <= 0 || end_8f <= 0) {
+        messagebox_common("注意！", "开始层和目的层均不能为负层。");
+        return;
+    }
 
-                // 初始化是否到过楼层的数组
-                for (int i = 0; i <= total_8f; i++) {
-                    f_8f[i] = false;
-                    k_8f[i] = 0;  // 初始化每层上下楼层数数组
-                }
+    if (start_8f > total_8f || end_8f > total_8f) {
+        messagebox_common("注意！", "开始层和目的层均不能超出总层数。");
+        return;
+    }
 
-                int j = 0;
+    // 释放和重塑楼层数据
+    delete[] f_8f;
+    delete[] k_8f;
+    f_8f = new bool[total_8f + 1]{false};
+    k_8f = new int[total_8f + 1]{0};
 
-                // 将第i层上下层数依次赋值给数组k
-                for (int i = 1; i <= total_8f; i++) {
-                    k_8f[i] = floors_vector[i - 1]->value();
-                    // 若第i层上下层数小于0
-                    if (k_8f[i] < 0) {
-                        messagebox_common("注意！", "每层上下楼层数必须大于等于0。");
-                        ++j;
-                        break;
-                    }
-
-                    int max = i > total_8f - i ? i - 1 : total_8f - i;  // 第i层能上或下楼层数的较大值
-
-                    // 第i层上下楼层数超出楼层上下范围
-                    if (k_8f[i] > max) {
-                        messagebox_common("注意！", QString("第").append(QString::number(i)).append("层所赋上下层数超出限度"));
-                        ++j;
-                        break;
-                    }
-                }
-
-                // 可以运算
-                if (j == 0) {
-                    int                         result_floor_8f = -1;
-                    int                         start           = 0;
-                    std::queue<QPair<int, int>> q;  // 定义队列
-                    QPair<int, int> p, t;  // p.first表示第几层，p.second表示按了几次按钮，t负责接受路径执行时p.first和p.second变化后的数值
-                    p.first  = start_8f;
-                    p.second = 0;       // 给p.first赋初值，就是开始层，给p.second赋值0，就是表示按按钮的次数
-                    q.push(p);          // 进入队列
-                    while (!q.empty())  // 队列非空
-                    {
-                        start++;
-                        p = q.front();                // 返回到结点在某条路径上的前一个，参与路径选择
-                        q.pop();                      // 弹出队列的第一个元素，队列长度减1
-                        if (f_8f[p.first] == true) {  // 如果楼层访问过（如果f[p.first]为true/真）
-                            if (p.first == end_8f) {
-                                result_floor_8f = p.second;
-                                break;
-                            }
-                            continue;
-                        }
-                        f_8f[p.first] = true;   // 标记楼层访问过
-                        if (p.first == end_8f)  // 到达目标楼层
-                        {
-                            if (start > 1) {
-                                result_floor_8f = p.second;  // 返回需按按钮的次数
-                                break;
-                            }
-                        }
-
-                        // 向下按，若不会下到负楼层，那么执行下面的一条代码段
-                        if (p.first - k_8f[p.first] > 0) {
-                            t.first  = p.first - k_8f[p.first];  // 当前按按钮之后，到了第几层
-                            t.second = p.second + 1;
-                            q.push(t);  // 当前按按钮的次数参与接下来的q队列while循环
-                        }
-
-                        // 向上按，若不会超出范围，那么执行下面的一条代码段
-                        if (p.first + k_8f[p.first] <= total_8f) {
-                            t.first  = p.first + k_8f[p.first];  // 当前按按钮之后，到了第几层
-                            t.second = p.second + 1;             // 按钮次数加1
-                            q.push(t);                           // 当前按按钮的次数参与接下来的q队列while循环
-                        }
-                    }
-                    if (result_floor_8f == -1) {
-                        messagebox_common("注意！", "指定条件无法到达。");
-                    } else {
-                        messagebox_common("Congratulations!",
-                                          QString("最少需要按").append(QString::number(result_floor_8f)).append("次按钮就能到达指定楼层。"));
-                    }
-                }
-            }
+    // 验证上下楼层值
+    for (int i = 1; i <= total_8f; ++i) {
+        k_8f[i] = floors_vector[i - 1]->value();
+        if (k_8f[i] < 0) {
+            messagebox_common("注意！", "每层上下楼层数必须大于等于0。");
+            return;
         }
 
-        // 按按钮2后，重新输入了不同的新总楼层数
-        else {
-            messagebox_common("注意！", "您已经更换了总楼层，请先点击按钮2得到可赋值的楼层！");
+        int max = std::max(i - 1, total_8f - i);
+        if (k_8f[i] > max) {
+            messagebox_common("注意！", QString("第").append(QString::number(i)).append("层所赋上下层数超出限度"));
+            return;
         }
     }
+
+    // 到达目的层的最少按按钮次数
+    std::queue<QPair<int, int>> q;
+    q.push({start_8f, 0});
+
+    while (!q.empty()) {
+        auto [current_floor, press_count] = q.front();
+        q.pop();
+
+        if (f_8f[current_floor]) {
+            continue;
+        }
+
+        f_8f[current_floor] = true;
+
+        if (current_floor == end_8f) {
+            messagebox_common("Congratulations!", QString("最少需要按").append(QString::number(press_count)).append("次按钮就能到达指定楼层。"));
+            return;
+        }
+
+        int next_floor_down = current_floor - k_8f[current_floor];
+        if (next_floor_down > 0 && !f_8f[next_floor_down]) {
+            q.push({next_floor_down, press_count + 1});
+        }
+
+        int next_floor_up = current_floor + k_8f[current_floor];
+        if (next_floor_up <= total_8f && !f_8f[next_floor_up]) {
+            q.push({next_floor_up, press_count + 1});
+        }
+    }
+
+    messagebox_common("注意！", "指定条件无法到达。");
 }
 
-// 重新选定新的总楼层数，改变对应控件状态
+// 控制可显示的上下楼层
 void efloors::floor_1()
 {
     int total_8f_cur = ui->li_1->text().toInt();
     if (total_8f < total_8f_cur) {
-        // 显示增加了的上下楼层数控件
         for (int i = total_8f; i < total_8f_cur; i++) {
             floors_vector[i]->show();
         }
     } else if (total_8f > total_8f_cur) {
-        // 隐藏多余的上下楼层数控件
         for (int i = total_8f_cur; i < total_8f; i++) {
             floors_vector[i]->close();
         }
@@ -231,6 +201,7 @@ void efloors::on_btn_confirm_clicked()
 {
     QString qtotal_8f      = ui->li_1->text();
     int     total_8f_child = qtotal_8f.toInt();
+
     if (qtotal_8f.isEmpty() || total_8f_child <= 0 || total_8f_child > 8) {
         total_8f = 0;
         if (qtotal_8f.isEmpty()) {
@@ -251,7 +222,7 @@ void efloors::on_btn_confirm_clicked()
     }
 }
 
-// 控件字体自适应窗口缩放
+// 自适应调整控件字体
 void efloors::resizeEvent(QResizeEvent *event)
 {
     int width  = this->width();
@@ -274,7 +245,6 @@ void efloors::resizeEvent(QResizeEvent *event)
     }
     font_default.setPointSize(origin_count);
 
-    // 重设标签字体
     for (QLabel *label : label_vector) {
         label->setFont(font_default);
     }
@@ -283,28 +253,27 @@ void efloors::resizeEvent(QResizeEvent *event)
     ui->btn_cancel->setFont(font_default);
 }
 
-void efloors::closeEvent(QCloseEvent *event)
-{
-    QMessageBox *box = new QMessageBox(QMessageBox::Information, "再见！", "谢谢使用！", QMessageBox::NoButton, this);
-    QTimer::singleShot(3000, box, SLOT(accept()));
-    box->setFont(font_default);
-    box->exec();
-    delete box;
-}
-
-// 键盘按键，控制按钮
+// 处理键盘按压事件
 void efloors::keyPressEvent(QKeyEvent *event)
 {
-    if (event->key() == Qt::Key_Return) {
-        QWidget *cur_wid = QApplication::focusWidget();
-        if (cur_wid == ui->btn_cancel) {
-            ui->btn_cancel->clicked();
-        } else if (cur_wid == ui->btn_calculate) {
-            on_btn_calculate_clicked();
-        } else if (cur_wid == ui->btn_confirm) {
-            on_btn_confirm_clicked();
+    switch (event->key()) {
+        case Qt::Key_Return: {
+            QWidget *cur_wid = QApplication::focusWidget();
+            if (cur_wid == ui->btn_cancel) {
+                ui->btn_cancel->click();
+            } else if (cur_wid == ui->btn_calculate) {
+                on_btn_calculate_clicked();
+            } else if (cur_wid == ui->btn_confirm) {
+                on_btn_confirm_clicked();
+            }
+            break;
         }
-    } else if (event->key() == Qt::Key_Escape) {
-        ui->btn_cancel->clicked();
+        case Qt::Key_Escape: {
+            ui->btn_cancel->click();
+            break;
+        }
+        default:
+            QWidget::keyPressEvent(event);
+            break;
     }
 }
