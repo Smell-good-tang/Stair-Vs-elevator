aa4067a38ce93ccec8a48bb699021439d038fafd
Smell-good-tang <155508961+Smell-good-tang@users.noreply.github.com>
2024-08-04 08:56:13 +0800

    Refining...
    窗口放在前台并获得焦点

--- StairVselevator/main.cpp
+++ StairVselevator/main.cpp
@@ -7,14 +7,29 @@
 #include "vld.h"
 #endif
 
+#include "Windows.h"
+#pragma comment(lib, "user32.lib")  // 添加这行以显式链接 user32.lib
+
 int main(int argc, char *argv[])
 {
     QApplication       a(argc, argv);
     efloors           *e              = new efloors();
     SingleInstanceApp *singleInstance = new SingleInstanceApp(e);
     QObject::connect(singleInstance, &SingleInstanceApp::showExistingWindow, e, [=]() {
-        e->activateWindow();
-        e->show();
+        HWND  hForeWnd = NULL;
+        HWND  hWnd     = HWND(e->winId());
+        DWORD dwForeID;
+        DWORD dwCurID;
+
+        hForeWnd = GetForegroundWindow();
+        dwCurID  = GetCurrentThreadId();
+        dwForeID = GetWindowThreadProcessId(hForeWnd, NULL);
+        AttachThreadInput(dwCurID, dwForeID, TRUE);
+        ShowWindow(hWnd, SW_SHOWNORMAL);
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+        SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+        SetForegroundWindow(hWnd);
+        AttachThreadInput(dwCurID, dwForeID, FALSE);
     });
     e->show();
     return a.exec();
