c016205ba315439dbc0eee0ee17014fc24814bf6
Smell-good-tang <155508961+Smell-good-tang@users.noreply.github.com>
2024-08-04 20:26:01 +0800

    Refining...
    Refining...

--- StairVselevator-2/main.cpp
+++ StairVselevator-2/main.cpp
@@ -16,20 +16,25 @@ int main(int argc, char *argv[])
     hfloors           *h              = new hfloors();
     SingleInstanceApp *singleInstance = new SingleInstanceApp(h);
     QObject::connect(singleInstance, &SingleInstanceApp::showExistingWindow, h, [=]() {
-        HWND  hForeWnd = NULL;
-        HWND  hWnd     = HWND(h->winId());
-        DWORD dwForeID;
-        DWORD dwCurID;
+        if (!h->isHidden()) {
+            HWND  hForeWnd = NULL;
+            HWND  hWnd     = HWND(h->winId());
+            DWORD dwForeID;
+            DWORD dwCurID;
 
-        hForeWnd = GetForegroundWindow();
-        dwCurID  = GetCurrentThreadId();
-        dwForeID = GetWindowThreadProcessId(hForeWnd, NULL);
-        AttachThreadInput(dwCurID, dwForeID, TRUE);
-        ShowWindow(hWnd, SW_SHOWNORMAL);
-        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
-        SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
-        SetForegroundWindow(hWnd);
-        AttachThreadInput(dwCurID, dwForeID, FALSE);
+            hForeWnd = GetForegroundWindow();
+            dwCurID  = GetCurrentThreadId();
+            dwForeID = GetWindowThreadProcessId(hForeWnd, NULL);
+            AttachThreadInput(dwCurID, dwForeID, TRUE);
+            ShowWindow(hWnd, SW_SHOWNORMAL);
+            SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+            SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+            SetForegroundWindow(hWnd);
+            AttachThreadInput(dwCurID, dwForeID, FALSE);
+        } else {
+            h->activateWindow();
+            h->show();
+        }
     });
     h->show();
     return a.exec();
